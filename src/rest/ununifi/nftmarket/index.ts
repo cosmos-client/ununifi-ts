991679c9606b0c520eb8a5a8c42fdbf65caec792
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-21 17:15:36 +0900

    feat: nftmodule tx related msg codec
    Current Msg codec refer the following version.
    
    Commit id
    https://github.com/UnUniFi/chain/commit/a5066a30fadd3e6e6d4a77b40707b6e62373cbe5
    
    Branch
    https://github.com/UnUniFi/chain/tree/nftmarket_implementation
    
    Currently, because no swagger.yml changes, we can't implement query in module.ts
    
    After swagger.yml changed, we need to implement query in module.ts and enable export of query.

---
+++ src/rest/ununifi/nftmarket/index.ts
@@ -0,0 +1,54 @@
+import { ununifi } from "../../../proto";
+import { cosmosclient } from "@cosmos-client/core";
+
+// Todo: after swagger.yml change, create module.ts and implement and enable next line.
+// export * as nftmarket from "./module";
+
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgListNft",
+  ununifi.nftmarket.MsgListNft
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgCancelNftListing",
+  ununifi.nftmarket.MsgCancelNftListing
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgExpandListingPeriodResponse",
+  ununifi.nftmarket.MsgExpandListingPeriodResponse
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgPlaceBidResponse",
+  ununifi.nftmarket.MsgPlaceBidResponse
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgCancelBid",
+  ununifi.nftmarket.MsgCancelBid
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgEndNftListing",
+  ununifi.nftmarket.MsgEndNftListing
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgPayFullBid",
+  ununifi.nftmarket.MsgPayFullBid
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgBorrow",
+  ununifi.nftmarket.MsgBorrow
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgRepay",
+  ununifi.nftmarket.MsgRepay
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgMintStableCoin",
+  ununifi.nftmarket.MsgMintStableCoin
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgBurnStableCoin",
+  ununifi.nftmarket.MsgBurnStableCoin
+);
+cosmosclient.codec.register(
+  "/ununifi.nftmarket.MsgLiquidate",
+  ununifi.nftmarket.MsgLiquidate
+);
