52dbd0ea9b17083f69459a42a32c15ca31b08f13
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-28 18:59:41 +0900

    feat: cdn and bump @cosmos-client/core v0.46.0-rc5 ununifi-client v0.46.0-rc3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export * from './config';
-export * from './rest';
-export * from './proto';
+import * as ununifi from "./module";
+import Long from "long";
+import * as $protobuf from "protobufjs/minimal";
+
+$protobuf.util.Long = Long;
+$protobuf.configure();
+
+export default ununifi;
