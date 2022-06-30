ed349125da8e2a6354fb57ec2cbc34b5444c1ba8
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-30 14:25:44 +0900

    fix: bump @cosmos-client/core v0.46.0-rc7 and do not overwrite existing codec

--- src/cdn.ts
+++ src/cdn.ts
@@ -5,18 +5,7 @@ import Long from 'long';
 (window as any).ununificlient = ununificlient;
 
 if ((window as any).cosmosclient) {
-  (window as any).cosmosclient.config.codecMaps.constructor = {
-    ...(window as any).cosmosclient.config.codecMaps.constructor,
-    ...cosmosclient.config.codecMaps.constructor,
-  };
-
-  for (const [key, value] of cosmosclient.config.codecMaps.convertJSON.entries()) {
-    (window as any).cosmosclient.config.codecMaps.convertJSON.set(key, value);
-  }
-
-  for (const [key, value] of cosmosclient.config.codecMaps.inv.entries()) {
-    (window as any).cosmosclient.config.codecMaps.inv.set(key, value);
-  }
+  (window as any).cosmosclient.config.mergeCodecMaps(cosmosclient.config.codecMaps);
 } else {
   (window as any).Long = Long;
   (window as any).cosmosclient = cosmosclient;
