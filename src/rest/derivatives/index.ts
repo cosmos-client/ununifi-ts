7fe022eb29b90e10861e7b9464c20d6fa7fcffde
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-01-26 13:47:30 +0900

    feat: derivatives module

---
+++ src/rest/derivatives/index.ts
@@ -0,0 +1,9 @@
+import { ununifi } from '../../proto';
+import cosmosclient from '@cosmos-client/core';
+
+export * as derivatives from './module';
+
+cosmosclient.codec.register('/ununifi.derivatives.MsgMintLiquidityProviderToken', ununifi.derivatives.MsgMintLiquidityProviderToken);
+cosmosclient.codec.register('/ununifi.derivatives.MsgBurnLiquidityProviderToken', ununifi.derivatives.MsgBurnLiquidityProviderToken);
+cosmosclient.codec.register('/ununifi.derivatives.MsgOpenPosition', ununifi.derivatives.MsgOpenPosition);
+cosmosclient.codec.register('/ununifi.derivatives.MsgClosePosition', ununifi.derivatives.MsgClosePosition);
