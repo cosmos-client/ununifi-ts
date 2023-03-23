6fb04f1f2d1752e8ba1b1e9f61b5929cc5905401
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-23 22:13:23 +0900

    feat: plz revert delete some module

--- src/rest/ecosystem-incentive/index.ts
+++
@@ -1,8 +0,0 @@
-import { ununifi } from '../../proto';
-import cosmosclient from '@cosmos-client/core';
-
-export * as ecosystemIncentive from './module';
-
-cosmosclient.codec.register('/ununifi.ecosystemincentive.MsgRegister', ununifi.ecosystemincentive.MsgRegister);
-cosmosclient.codec.register('/ununifi.ecosystemincentive.MsgWithdrawAllRewards', ununifi.ecosystemincentive.MsgWithdrawAllRewards);
-cosmosclient.codec.register('/ununifi.ecosystemincentive.MsgWithdrawReward', ununifi.ecosystemincentive.MsgWithdrawReward);
