252cc56fd89c1c3db54a09122fd2d940bb7b96d1
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-23 23:17:09 +0900

    feat: strategy weight

--- src/rest/yield-aggregator/index.ts
+++ src/rest/yield-aggregator/index.ts
@@ -6,5 +6,6 @@ export * as yieldAggregator from './module';
 cosmosclient.codec.register('/ununifi.yieldAggregator.MsgDepositToVault', ununifi.chain.yieldaggregator.MsgDepositToVault);
 cosmosclient.codec.register('/ununifi.yieldAggregator.MsgWithdrawFromVault', ununifi.chain.yieldaggregator.MsgWithdrawFromVault);
 cosmosclient.codec.register('/ununifi.yieldAggregator.MsgCreateVault', ununifi.chain.yieldaggregator.MsgCreateVault);
+cosmosclient.codec.register('/ununifi.yieldAggregator.StrategyWeight', ununifi.chain.yieldaggregator.StrategyWeight);
 cosmosclient.codec.register('/ununifi.yieldAggregator.MsgMsgDeleteVault', ununifi.chain.yieldaggregator.MsgDeleteVault);
 cosmosclient.codec.register('/ununifi.yieldAggregator.MsgTransferVaultOwnership', ununifi.chain.yieldaggregator.MsgTransferVaultOwnership);
