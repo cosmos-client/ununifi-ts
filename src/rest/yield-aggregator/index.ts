442aaf3f2975ea91175de0e230e5f93af6467a52
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-30 21:23:13 +0900

    feat: fix msg

--- src/rest/yield-aggregator/index.ts
+++ src/rest/yield-aggregator/index.ts
@@ -7,7 +7,7 @@ cosmosclient.codec.register('/ununifi.chain.yieldaggregator.MsgDepositToVault', 
 cosmosclient.codec.register('/ununifi.chain.yieldaggregator.MsgWithdrawFromVault', ununifi.chain.yieldaggregator.MsgWithdrawFromVault);
 cosmosclient.codec.register('/ununifi.chain.yieldaggregator.MsgCreateVault', ununifi.chain.yieldaggregator.MsgCreateVault);
 cosmosclient.codec.register('/ununifi.chain.yieldaggregator.StrategyWeight', ununifi.chain.yieldaggregator.StrategyWeight);
-cosmosclient.codec.register('/ununifi.chain.yieldaggregator.MsgMsgDeleteVault', ununifi.chain.yieldaggregator.MsgDeleteVault);
+cosmosclient.codec.register('/ununifi.chain.yieldaggregator.MsgDeleteVault', ununifi.chain.yieldaggregator.MsgDeleteVault);
 cosmosclient.codec.register(
   '/ununifi.chain.yieldaggregator.MsgTransferVaultOwnership',
   ununifi.chain.yieldaggregator.MsgTransferVaultOwnership,
