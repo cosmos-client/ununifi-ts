77429002f3cbf934168bc4ebc1da1500fdfdd2f2
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-11 20:51:58 +0900

    fix: module

--- src/rest/copy-trading/index.ts
+++ src/rest/copy-trading/index.ts
@@ -1,7 +1,7 @@
 import { ununifi } from '../../proto';
 import cosmosclient from '@cosmos-client/core';
 
-export * as copyTrading from '../derivatives/module';
+export * as copyTrading from '../copy-trading/module';
 
 cosmosclient.codec.register('/ununifi.copytrading.MsgCreateExemplaryTrader', ununifi.copytrading.MsgCreateExemplaryTrader);
 cosmosclient.codec.register('/ununifi.copytrading.MsgUpdateExemplaryTrader', ununifi.copytrading.MsgUpdateExemplaryTrader);
