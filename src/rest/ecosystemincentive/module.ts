46da9ebedc060d1deeb40bb4a6106c2a4e601377
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-01 19:14:45 +0900

    feat: merge #32

--- src/rest/ecosystemincentive/module.ts
+++ src/rest/ecosystemincentive/module.ts
@@ -5,6 +5,10 @@ export function params(sdk: cosmosclient.CosmosSDK) {
   return new QueryApi(undefined, sdk.url).ecosystemIncentiveParams();
 }
 
-export function reward(sdk: cosmosclient.CosmosSDK, address: string, denom?: string) {
+export function ecosystemRewards(sdk: cosmosclient.CosmosSDK, address: string, denom?: string) {
   return new QueryApi(undefined, sdk.url).ecosystemRewards(address, denom);
+}
+
+export function recipientAddressWithNftId(sdk: cosmosclient.CosmosSDK, classId: string, nftId: string) {
+  return new QueryApi(undefined, sdk.url).recipientAddressWithNftId(classId, nftId);
 }
--- src/rest/nftbackedloan/module.ts
+++ src/rest/nftbackedloan/module.ts
@@ -17,19 +17,15 @@ export function listedClasses(sdk: cosmosclient.CosmosSDK, nftLimit?: number) {
   return new QueryApi(undefined, sdk.url).listedClasses(nftLimit);
 }
 
-export function listedClass(sdk: cosmosclient.CosmosSDK, classId: string, nftLimit: number) {
+export function listedClass(sdk: cosmosclient.CosmosSDK, classId: string, nftLimit?: number) {
   return new QueryApi(undefined, sdk.url).listedClass(classId, nftLimit);
 }
-
-// export function loans(sdk: cosmosclient.CosmosSDK) {
-//   return new QueryApi(undefined, sdk.url).loans();
-// }
 
 export function loan(sdk: cosmosclient.CosmosSDK, classId: string, nftId: string) {
   return new QueryApi(undefined, sdk.url).loan(classId, nftId);
 }
 
-export function nftBids(sdk: cosmosclient.CosmosSDK, classId: string, nftId: string) {
+export function nftBids(sdk: cosmosclient.CosmosSDK, classId?: string, nftId?: string) {
   return new QueryApi(undefined, sdk.url).nftBids(classId, nftId);
 }
 
@@ -40,10 +36,6 @@ export function bidderBids(sdk: cosmosclient.CosmosSDK, bidder: string) {
 export function liquidation(sdk: cosmosclient.CosmosSDK, classId: string, nftId: string) {
   return new QueryApi(undefined, sdk.url).liquidation(classId, nftId);
 }
-
-// export function paymentStatus(sdk: cosmosclient.CosmosSDK, classId: string, nftId: string, bidder: string) {
-//   return new QueryApi(undefined, sdk.url).paymentStatus(classId, nftId, bidder);
-// }
 
 export function rewards(sdk: cosmosclient.CosmosSDK, address: string) {
   return new QueryApi(undefined, sdk.url).rewards(address);
