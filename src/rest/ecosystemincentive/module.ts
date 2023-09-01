6d41fcdea986e31c8d0e23dbd2030f96e9011ba8
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-01 18:57:07 +0900

    fix: query endpoint

--- src/rest/ecosystemincentive/module.ts
+++ src/rest/ecosystemincentive/module.ts
@@ -5,10 +5,6 @@ export function params(sdk: cosmosclient.CosmosSDK) {
   return new QueryApi(undefined, sdk.url).ecosystemIncentiveParams();
 }
 
-export function reward(sdk: cosmosclient.CosmosSDK, subjectAddr: string, denom: string) {
-  return new QueryApi(undefined, sdk.url).reward(subjectAddr, denom);
-}
-
-export function allRewards(sdk: cosmosclient.CosmosSDK, subjectAddr: string) {
-  return new QueryApi(undefined, sdk.url).allRewards(subjectAddr);
+export function reward(sdk: cosmosclient.CosmosSDK, address: string, denom?: string) {
+  return new QueryApi(undefined, sdk.url).ecosystemRewards(address, denom);
 }
